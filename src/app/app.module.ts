05144c85d8889d599c272321e3d6ba32da15acd1
Leandro J Martinez <leandro666m@gmail.com>
2022-12-29 11:24:26 -0300

    terminado. (quedaria los media queries para mobile)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './Components/about/about.component';
@@ -22,7 +24,9 @@ import { SkillsComponent } from './Components/skills/skills.component';
     SkillsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxTypedJsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
